1078a997b6ced6c3d8ef4eed45d2f7a68c8c06da
Hippasusss <dannyherbertgj@gmail.com>
2026-03-10 16:53:50 +0000

    added decdicated set colour

--- script/Feedblock.cs
+++ script/Feedblock.cs
@@ -15,4 +15,10 @@ using System;
 public partial class Feedblock : Sprite2D
 {
 	public HumanStats stats;
+
+    public void SetColour(float r, float g, float b, float a)
+    {
+        var newColour = new Color(r,g,b,a);
+        Modulate = newColour;
+    }
 }

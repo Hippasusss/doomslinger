9cf3acb48fc69e433bafcc409a810c0940a3d0a8
Hippasusss <dannyherbertgj@gmail.com>
2026-03-19 01:15:57 +0000

    updated to use new timer and Feedblock color setting

--- script/Feed.cs
+++ script/Feed.cs
@@ -9,11 +9,14 @@
 
 using Godot;
 using System;
+using System.Text;
 using System.Collections.Generic;
+using Utils;
 
 public partial class Feed : Node2D 
 {
     public Action<Feedblock> newMainFeedBlockCallBack;
+    public List<Feedblock> FeedBlocks => feedBlocks;
 
     private const int numBlocks = 12;
     private const int blockSpacing = 52;
@@ -21,6 +24,7 @@ public partial class Feed : Node2D
     [Export] private ColorPalette feedBlockPalette;
     [Export] private Light2D screenLight;
     private readonly List<Feedblock> feedBlocks = [];
+    private bool enabled = true;
 
     public override void _Ready()
     {
@@ -28,23 +32,21 @@ public partial class Feed : Node2D
         {
             Feedblock newfeedblock = feedBlockSN.Instantiate<Feedblock>();
             AddChild(newfeedblock);
-            feedBlocks.Add(newfeedblock);
+            FeedBlocks.Add(newfeedblock);
             ResetBlock(newfeedblock);
             if (CheckBlockDelta(newfeedblock, 0))
             {
-                screenLight.Color = newfeedblock.Modulate;
+                screenLight.Color = newfeedblock.GetColour();
             }
         }
     }
- 
-    private double timer = 1;
+
+    private readonly DeltaTimer swipeTimer = new(1, 4);
     public override void _Process(double delta) 
     {
-        timer -= delta;
-        if(timer <= 0)
+        if(swipeTimer.Delta(delta) && enabled)
         {
             AdvanceFeed();
-            timer = GD.RandRange(1.0, 4.0);
         }
     }
 
@@ -53,7 +55,7 @@ public partial class Feed : Node2D
         Tween tween = CreateTween().SetParallel(true);
         float duration = 0.4f;
 
-        foreach (Feedblock block in feedBlocks)
+        foreach (Feedblock block in FeedBlocks)
         {
             tween.TweenProperty(block, "position:y", block.Position.Y + blockSpacing, duration)
                 .SetTrans(Tween.TransitionType.Quart)
@@ -61,7 +63,7 @@ public partial class Feed : Node2D
 
             if (CheckBlockDelta(block, -1))
             {
-                tween.TweenProperty(screenLight, "color", block.Modulate, duration)
+                tween.TweenProperty(screenLight, "color", block.GetColour(), duration)
                     .SetTrans(Tween.TransitionType.Quart)
                     .SetEase(Tween.EaseType.Out);
             }
@@ -71,7 +73,7 @@ public partial class Feed : Node2D
 
         tween.Chain().TweenCallback(Callable.From(() =>
         {
-            foreach (Feedblock block in feedBlocks)
+            foreach (Feedblock block in FeedBlocks)
             {
                 if (block.Position.Y > 100)
                 {
@@ -85,20 +87,69 @@ public partial class Feed : Node2D
         }));
     }
 
+    public string GetBlockData()
+    {
+        if (FeedBlocks.Count == 0) return string.Empty;
+
+        StringBuilder text = new();
+        int count = FeedBlocks.Count;
+
+        for (int i = 0; i < count; i++)
+        {
+            int currentIndex = (feedBlocks.IndexOf(GetBlockBeingRead()) - i + count) % count;
+
+            Feedblock block = FeedBlocks[currentIndex];
+
+            text.AppendLine(block.stats.GetStatsString());
+        }
+
+        return text.ToString();
+    }
+
+    public void ToggleOnOff(bool onOff)
+    {
+        enabled = onOff;
+        if(onOff)
+        {
+            Tween tween = CreateTween();
+            tween.TweenProperty(this, "modulate", Colors.White, 0.4f)
+                .SetTrans(Tween.TransitionType.Quart)
+                .SetEase(Tween.EaseType.Out);
+        }
+        else
+        {
+            const float darkness = 0.2f;
+            Color dark = new(darkness,darkness,darkness,1);
+            Tween tween = CreateTween();
+            tween.TweenProperty(this, "modulate", dark, 0.4f)
+                .SetTrans(Tween.TransitionType.Quart)
+                .SetEase(Tween.EaseType.Out);
+            // Modulate = dark;
+        }
+    }
+
     private static bool CheckBlockDelta(Feedblock block, int delta)
     {
         if (Mathf.Abs(block.Position.Y + (blockSpacing * -delta)) < 1.0f) return true;
         return false;
     }
 
+    private Feedblock GetBlockBeingRead()
+    {
+        foreach(Feedblock block in feedBlocks)
+        {
+            if(CheckBlockDelta(block, 0))
+            {
+                return block;
+            }
+        }
+        return null;
+    }
+
     private void ResetBlock(Feedblock block)
     {
-
-        block.Position = new Vector2(block.Position.X, (feedBlocks.Count - 2) * -blockSpacing);
+        block.Position = new Vector2(block.Position.X, (FeedBlocks.Count - 2) * -blockSpacing);
         block.SetColour(feedBlockPalette.Colors.GetRandom());
         block.stats.RandomizeStats();
     }
-
-
-
 }
--- script/Feedblock.cs
+++ script/Feedblock.cs
@@ -14,15 +14,22 @@ using Godot;
 public partial class Feedblock : Sprite2D
 {
     public HumanStats stats;
+    [Export] private Sprite2D colourSprite;
 
     public void SetColour(float r, float g, float b, float a)
     {
         var newColour = new Color(r,g,b,a);
-        Modulate = newColour;
+        colourSprite.Modulate = newColour;
     }
 
     public void SetColour(Color color)
     {
-        Modulate = color;
+        colourSprite.Modulate = color;
     }
+
+    public Color GetColour()
+    {
+        return colourSprite.Modulate;
+    }
+
 }

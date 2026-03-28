53ff96afc2a4adc862f804a7a092807c73cff92d
Hippasusss <dannyherbertgj@gmail.com>
2026-03-28 04:09:39 +0000

    extension for drawing a frame using a percentage. Higher frame for higher percent

--- script/Extensions.cs
+++ script/Extensions.cs
@@ -21,3 +21,20 @@ public static class EnumerableExtensions
         return list[GD.RandRange(0, list.Count - 1)];
     }
 }
+
+public static class SpriteExtensions
+{
+    public static void SetFrameFromPercent(this Sprite2D source, float percent)
+    {
+        if (percent < float.Epsilon)
+        {
+            source.Visible = false;
+            source.Frame = 0;
+        }
+        else
+        {
+            source.Visible = true;
+            source.Frame = (int)Mathf.Floor((float)((source.Hframes - 1) * percent));
+        }
+    }
+}

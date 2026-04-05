0e78358c50ed74c0b6588df7c542d70fc1c0b14a
Hippasusss <dannyherbertgj@gmail.com>
2026-04-05 06:39:37 +0100

    added start, stop, and reset. and renamed some vars for clarity

--- script/Timer.cs
+++ script/Timer.cs
@@ -9,6 +9,7 @@ namespace Utils
         private double _maxTime;
         private double _currentTime;
         private readonly Random _random = new();
+        private bool isRunning = true;
 
         public DeltaTimer(double setTime)
         {
@@ -21,45 +22,62 @@ namespace Utils
         {
             _minTime = minTime;
             _maxTime = maxTime;
-            _currentTime = GetNextRandomTime();
+            Reset();
         }
 
         public bool Delta(double removeTime)
         {
+            if(!isRunning) return false;
             _currentTime -= removeTime;
             if (_currentTime <= 0)
             {
-                _currentTime = GetNextRandomTime();
+                Reset();
                 return true;
             }
             return false;
         }
 
 
-        public void SetResetTime(double time, bool resetCurrent = false)
+        public void SetResetTime(double time, bool forceImmediateReset = false)
         {
-            SetResetRange(time, time, resetCurrent);
+            SetResetRange(time, time, forceImmediateReset);
         }
 
-        public void SetResetRange(double minTime, double maxTime, bool resetCurrent = false)
+        public void SetResetRange(double minTime, double maxTime, bool forceImmediateReset = false)
         {
             _minTime = minTime;
             _maxTime = maxTime;
-            if (resetCurrent)
+            if (forceImmediateReset)
             {
-                _currentTime = GetNextRandomTime();
+                Reset();
             }
+        }
+
+        public void Reset()
+        {
+            if (Math.Abs(_minTime - _maxTime) < 0.0001) 
+            {
+                _currentTime = _minTime; 
+            }
+            else
+            {
+                _currentTime = _random.NextDouble() * (_maxTime - _minTime) + _minTime;
+            }
+        }
+
+        public void Start()
+        {
+            if (!isRunning) isRunning = true;
+        }
+
+        public void Stop()
+        {
+            if (isRunning) isRunning = false;
         }
 
         public void ForceFinish()
         {
             _currentTime = 0;
-        }
-
-        private double GetNextRandomTime()
-        {
-            if (Math.Abs(_minTime - _maxTime) < 0.0001) return _minTime;
-            return _random.NextDouble() * (_maxTime - _minTime) + _minTime;
         }
     }
 }

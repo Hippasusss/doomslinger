95eff8db7bfa4b882506df5883a5d3327cfe6785
Hippasusss <dannyherbertgj@gmail.com>
2026-04-14 12:02:57 +0100

    bits

--- script/NotificaitonDisplay.cs
+++ script/NotificaitonDisplay.cs
@@ -1,6 +1,30 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class NotificaitonDisplay : Control
 {
+    [Export] private RichTextLabel displayText;
+
+    public enum WarningType
+    {
+        Error,
+        Info,
+        Warning
+    }
+
+    private static readonly Dictionary<WarningType, string> WarningTypeNames = new ()
+    {
+        { WarningType.Info, ">: Warning: " },
+        { WarningType.Warning, "> Error: " },
+        { WarningType.Error, ">:" }
+    };
+
+    public void AddNotification(string notification, WarningType warningType = WarningType.Info)
+    {
+        string prepend = WarningTypeNames[warningType];
+        displayText.Text += prepend + notification;
+    }
+
 }
+

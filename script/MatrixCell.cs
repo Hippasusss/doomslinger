9e65d494dc90738e2218a20c54ee4eff84f4032b
Hippasusss <dannyherbertgj@gmail.com>
2026-05-11 23:53:57 +0100

    added matrixcell

---
+++ script/MatrixCell.cs
@@ -0,0 +1,22 @@
+using Godot;
+
+public partial class MatrixCell : PanelContainer
+{
+
+    [Export] private Button button;
+    [Export] private RichTextLabel text;
+
+    public Button Button {get => button;}
+    public RichTextLabel Text {get => text;}
+    public Color Color
+    {
+        get
+        {
+            return button.SelfModulate;
+        }
+        set
+        {
+            button.SelfModulate=value;
+        }
+    }
+}

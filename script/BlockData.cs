a2ccafbca7eb3f0e306cf4ea39d763447494891a
Hippasusss <dannyherbertgj@gmail.com>
2026-05-11 23:52:18 +0100

    added blockdata

---
+++ script/BlockData.cs
@@ -0,0 +1,9 @@
+using Godot;
+using System;
+
+public partial class BlockData(int stat1Set, int stat2Set, int stat3Set) : Node
+{
+    private int stat1 = stat1Set;
+    private int stat2 = stat2Set;
+    private int stat3 = stat3Set;
+}

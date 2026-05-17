a8153f8ce7284e6d59cb0cdb8eb456d0c213bd6d
Hippasusss <dannyherbertgj@gmail.com>
2026-05-17 03:07:17 +0100

    updated to use Timer and added offset

--- script/PhoneLight.cs
+++ script/PhoneLight.cs
@@ -8,26 +8,26 @@ public partial class PhoneLight : PointLight2D
     [Export] public float FlickerRange = 0.3f;
     [Export] public float FlickerSpeed = 10.0f;
 
-    private float _targetEnergy;
-    private float _currentFlickerTime;
+    [Export] public float EnergyOffset = 0f;
     private float baseEnergy;
+    private float _targetEnergy;
 
     public override void _Ready()
     {
+        flickerTimer = new(1/FlickerSpeed);
         _targetEnergy = Energy;
         baseEnergy = Energy;
     }
 
+    public DeltaTimer flickerTimer = new(1);
     public override void _Process(double delta)
     {
-        _currentFlickerTime += (float)delta;
-        
-        if (_currentFlickerTime >= 1.0f / FlickerSpeed)
+
+        if (flickerTimer.Delta(delta))
         {
             _targetEnergy = baseEnergy + (float)GD.RandRange(-FlickerRange, FlickerRange);
-            _currentFlickerTime = 0;
         }
 
-        Energy = Mathf.Lerp(Energy, _targetEnergy, (float)delta * FlickerSpeed);
+        Energy = Mathf.Lerp(Energy, _targetEnergy, (float)delta * FlickerSpeed) + EnergyOffset;
     }
 }

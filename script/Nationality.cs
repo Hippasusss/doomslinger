316860ab4806c1ca77c0afd4eadb6db7bf3ff52b
Hippasusss <dannyherbertgj@gmail.com>
2026-06-21 03:59:49 +0100

    updated nationalities to have a flag and a color for the human ID

--- script/Nationality.cs
+++ script/Nationality.cs
@@ -6,4 +6,6 @@ namespace DoomSlinger;
 public partial class Nationality : Resource
 {
     [Export] public string Name { get; set; }
+    [Export] public Color IDColor { get; set; }
+    [Export] public Texture2D Flag { get; set; }
 }

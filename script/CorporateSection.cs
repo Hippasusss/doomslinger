a9fef6957fbcedfcb768fb6cb60b1b4a11c84466
Hippasusss <dannyherbertgj@gmail.com>
2026-07-10 04:28:55 +0100

    added cutout to corposection

--- script/CorporateSection.cs
+++ script/CorporateSection.cs
@@ -7,6 +7,9 @@ public partial class CorporateSection : Panel, ISection
     [Export] private SectionRevealer revealer;
     [Export] private Panel notificationLight;
     [Export] private Label CreditAmountDisplay;
+    [Export] private CutoutContainer cutoutContainer;
+
+    private ShaderMaterial cutoutMaterial;
 
     public void Toggle() => revealer?.Toggle();
     public void SetOpen(bool open) => revealer?.SetOpen(open);
@@ -23,4 +26,26 @@ public partial class CorporateSection : Panel, ISection
         }
     }
 
+    public override void _Ready()
+    {
+        var shader = ResourceLoader.Load<Shader>("res://script/shader/cutout.gdshader");
+        cutoutMaterial = new ShaderMaterial { Shader = shader };
+        Material = cutoutMaterial;
+        UpdateCutoutUniform();
+    }
+
+    private void UpdateCutoutUniform()
+    {
+        var size = GetRect().Size;
+        var cutouts = cutoutContainer?.GetCutoutUVs(size) ?? new Vector4[8];
+
+        for (int i = 0; i < cutouts.Length; i++)
+            cutoutMaterial.SetShaderParameter($"cutout_{i}", cutouts[i]);
+
+        if (GetThemeStylebox("panel") is StyleBoxFlat stylebox)
+        {
+            cutoutMaterial.SetShaderParameter("border_color", stylebox.BorderColor);
+            cutoutMaterial.SetShaderParameter("border_width_uv", stylebox.BorderWidthLeft / size.X);
+        }
+    }
 }

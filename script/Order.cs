711a455c72e2d254fdacbf0783ae1a55080641cd
Hippasusss <dannyherbertgj@gmail.com>
2026-07-14 04:22:09 +0100

    added Order

---
+++ script/Order.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public partial class Order : Panel
+{
+    private enum Modifiers
+    {
+        DONOTALLOW,
+        REPORT,
+        REFUSE,
+    }
+
+
+    public override void _Ready()
+    {
+    }
+
+    public override void _Process(double delta)
+    {
+    }
+
+    public void GenerateNewOrder()
+    {
+
+    }
+}
